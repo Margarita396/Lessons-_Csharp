f69048027ece252b4c403b3a93fae7a38e3ebcb0
Margarita <mv_zelenskaya@mail.ru>
2022-08-24 23:40:40 +0400

    Example 004 Division

---
+++ Ex004_Div/Program.cs
@@ -0,0 +1,10 @@
+﻿double numberA;
+Console.Write("Введите число A: ");
+numberA = Convert.ToDouble(Console.ReadLine());
+
+double numberB;
+Console.Write("Введите число B: ");
+numberB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Частное равно " + (numberA / numberB));
+
